6a85ae9b4c05f22e7fbf3a3257f2a9e2eac7ce5c
firdaus <firdausxwell@gmail.com>
2023-06-13 20:39:24 +0800

    additional cleanup

--- project 1/app.js
+++ project 1/app.js
@@ -7,32 +7,56 @@ let result = 0;
 let hitPosition;
 let currentTime = 60;
 let timerId = null;
+let countDownTimerId = null;
 
-function randomSquare() {
-  //function that removes the mole class from all the squares and then adds the mole class to a random square
+randomSquare = () => {
   squares.forEach((square) => {
-    //forEach is a function that loops through all the squares
-    square.classList.remove("mole"); //removes the mole class from all the squares
+    square.removeEventListener("mousedown", removeMoleClass);
+    square.classList.remove("mole");
   });
 
-  let randomSquare = squares[Math.floor(Math.random() * 9)]; //random square is a variable that is assigned to a random square   Math.floor(Math.random() * 9) is a function that generates a random number between 0 and 8
-  randomSquare.classList.add("mole"); //adds the mole class to the random square
+  let randomSquareIndex = Math.floor(Math.random() * 9);
+  let randomSquare = squares[randomSquareIndex];
+  randomSquare.classList.add("mole");
+  randomSquare.addEventListener("mousedown", (event) =>
+    removeMoleClass(event.target)
+  );
 
-  hitPosition = randomSquare.id; //assigns the id of the random square to the hitPosition variable
-}
-squares.forEach((square) => {
-  //forEach is a function that loops through all the squares
-  square.addEventListener("mousedown", () => {
-    if (square.id == hitPosition) {
-      result++;
-      score.textContent = result;
-      hitPosition = null;
-    }
-  });
-});
+  hitPosition = randomSquare.id;
+};
 
-//function on start button
-startbtn.addEventListener("click", () => {
+removeMoleClass = (clickedSquare) => {
+  if (clickedSquare.id == hitPosition) {
+    result++;
+    score.textContent = result;
+    hitPosition = null;
+  }
+};
+
+moveMole = () => {
+  timerId = setInterval(randomSquare, 5000);
+};
+
+countDown = () => {
+  currentTime--;
+  timeLeft.textContent = currentTime;
+
+  if (currentTime <= 10) {
+    timeLeft.style.color = "red";
+  } else if (currentTime <= 20) {
+    timeLeft.style.color = "yellow";
+  } else if (currentTime <= 30) {
+    timeLeft.style.color = "green";
+  }
+
+  if (currentTime == 0) {
+    clearInterval(countDownTimerId);
+    clearInterval(timerId);
+    endGame();
+  }
+};
+
+startGame = () => {
   if (timerId) {
     clearInterval(timerId);
   }
@@ -43,33 +67,36 @@ startbtn.addEventListener("click", () => {
   result = 0;
   score.textContent = result;
   timeLeft.textContent = currentTime;
-});
+  timeLeft.style.color = "white";
+};
 
-function moveMole() {
-  //function that moves the mole around the board
-  timerId = setInterval(randomSquare, 5000);
-}
-
-moveMole();
-
-function countDown() {
-  currentTime--;
+resetGame = () => {
+  clearInterval(countDownTimerId);
+  clearInterval(timerId);
+  currentTime = 60;
+  result = 0;
+  score.textContent = result;
   timeLeft.textContent = currentTime;
+  timeLeft.style.color = "white";
+};
 
-  if (currentTime == 0) {
-  } else if (currentTime <= 10) {
-    timeLeft.style.color = "red";
-  } else if (currentTime <= 20) {
-    timeLeft.style.color = "yellow";
-  } else if (currentTime <= 30) {
-    timeLeft.style.color = "green";
-  } else if (currentTime == 0) {
-    clearInterval(countDownTimerId);
-    clearInterval(timerId);
-    //reset the timing
-    currentTime = 60;
-    alert("GAME OVER! Your final score is " + result);
-  }
-}
+endGame = () => {
+  alert("GAME OVER! Your final score is " + result);
+  clearInterval(countDownTimerId);
+  clearInterval(timerId);
+  resetGame();
+};
 
-let countDownTimerId = setInterval(countDown, 1000);
+// Event listeners
+startbtn.addEventListener("click", startGame);
+resetbtn.addEventListener("click", resetGame);
+
+// Initial game setup
+resetGame();
+
+// Add event listeners to squares after defining removeMoleClass
+squares.forEach((square) => {
+  square.addEventListener("mousedown", (event) =>
+    removeMoleClass(event.target)
+  );
+});
